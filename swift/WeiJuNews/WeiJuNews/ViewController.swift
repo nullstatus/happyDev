b8c3d94cfecf42da9972e458838fe1eb82a16207
nullStatus <dll2051@gmail.com>
2015-05-23 00:33:17 +0800

    add first swift app
    happy the first

---
+++ swift/WeiJuNews/WeiJuNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeiJuNews
+//
+//  Created by light on 15/5/23.
+//  Copyright (c) 2015年 tuotuode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
