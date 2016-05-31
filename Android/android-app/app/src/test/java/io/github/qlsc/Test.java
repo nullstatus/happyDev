a436b181fb9bea990268a5820936aad7e14ee914
dll2051 <dll2051@gmail.com>
2016-05-31 09:10:41 +0800

    commit

---
+++ Android/android-app/app/src/test/java/io/github/qlsc/Test.java
@@ -0,0 +1,12 @@
+package io.github.qlsc;
+
+import android.view.LayoutInflater;
+
+/**
+ * Created by qlsc on 2016/5/27.
+ */
+public class Test {
+    void test(){
+        LayoutInflater.from()
+    }
+}
